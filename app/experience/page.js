3a6ce68c3c2d5be9f957dc722b1c05c889bc6b8f
clarence00 <clarencenatividad12@gmail.com>
2025-01-27 12:54:00 +0800

    [UPDATE]: Career page added

--- app/experience/page.js
+++ app/experience/page.js
@@ -1,7 +1,90 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import { AnimatePresence, motion } from "framer-motion";
+import careerData from "@/data/career.json";
+import CareerCard from "@/components/CareerCard";
+// import TechStackCard from "../components/TechStackCard";
+// import careerData from "../assets/media/career.json";
+// import techStackData from "../assets/media/techstack.json";
+// import toolData from "../assets/media/tools.json";
 
 const Experience = () => {
-  return <div>EXPERIENCE</div>;
+  const [active, setActive] = useState("Career");
+
+  const handleSetActive = (menuItem) => {
+    setActive(menuItem);
+  };
+
+  const links = [
+    { name: "Career", path: "./experience" },
+    { name: "Tech Stacks", path: "./techStacks" },
+    { name: "Tools", path: "./tools" },
+  ];
+
+  return (
+    <div className="h-screen">
+      <div className="flex flex-col items-center mt-20">
+        <div className="flex mx-[15%] justify-center gap-12">
+          <div className="flex flex-col pt-16 gap-2">
+            <h1 className="text-3xl font-bold mx-auto mb-4">Experience</h1>
+            {links.map((item) => (
+              <Link
+                href={item.path}
+                key={item.name}>
+                <button
+                  className={`${
+                    active === item.name
+                      ? "btn-active text-primary-content"
+                      : ""
+                  } h-12 w-64 text-sm font-medium border border-accent btn-outline b text-accent rounded-2xl btn-accent`}
+                  onClick={() => handleSetActive(item.name)}>
+                  {item.name}
+                </button>
+              </Link>
+            ))}
+          </div>
+          <div className="w-[600px]">
+            <AnimatePresence mode="wait">
+              {active === "Career" && (
+                <motion.div
+                  key="Career"
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ delay: 0.5, duration: 0.8, ease: "easeInOut" }}>
+                  {careerData.map((data, index) => (
+                    <CareerCard
+                      key={index}
+                      {...data}
+                    />
+                  ))}
+                </motion.div>
+              )}
+              {/* {active === "Tech Stacks" && (
+                <motion.div
+                  key="Tech Stacks"
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ delay: 0.5, duration: 0.8, ease: "easeInOut" }}>
+                  <TechStackCard techData={techStackData} />
+                </motion.div>
+              )}
+              {active === "Tools" && (
+                <motion.div
+                  key="Tools"
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ delay: 0.5, duration: 0.8, ease: "easeInOut" }}>
+                  <TechStackCard techData={toolData} />
+                </motion.div>
+              )} */}
+            </AnimatePresence>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
---
+++ components/CareerCard.js
@@ -0,0 +1,66 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Icons from "./Icons";
+
+const CareerCard = ({
+  title,
+  href,
+  company,
+  duration,
+  details,
+  icons,
+  size,
+  position,
+}) => {
+  const [pointerEnabled, setPointerEnabled] = useState();
+
+  useEffect(() => {
+    setPointerEnabled(false);
+    const timer = setTimeout(() => {
+      setPointerEnabled(true);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div
+      className={`flex flex-col rounded-2xl mx-auto my-4 duration-200 hover:scale-105 hover:bg-base-300/[0.8] hover:shadow-lg hover:border hover: border-white/[0.1] ${
+        pointerEnabled ? "pointer-events-auto" : "pointer-events-none"
+      }`}>
+      <div className="card-body p-4 group">
+        <h2 className="card-title">{title}</h2>
+        <div className="flex justify-between italic -mt-2">
+          <a
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-sm hover:underline underline-offset-2">
+            {company}
+          </a>
+          <h6 className="text-sm">{duration}</h6>
+        </div>
+        <div className="divider my-0"></div>
+        <ul className="list-disc pl-8 max-h-0 text-sm opacity-0 duration-200 group-hover:opacity-100 group-hover:max-h-[100%]">
+          {details.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        <div className="divider -my-2 max-h-0 opacity-0 duration-200 group-hover:opacity-100 group-hover:max-h-[100%] group-hover:my-0"></div>
+        <div className="flex gap-4 px-2">
+          {icons.map((icon, index) => (
+            <Icons
+              key={index}
+              type={icon}
+              size={size}
+              position={position}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CareerCard;
